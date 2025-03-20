cd583313c415971154991f9d58f6c708bac2e061
arul2101 <myanuarullah20@gmail.com>
2025-03-20 12:48:11 +0700

    day 4 Staircase 21/03/2025

---
+++ #4-Staircase.js
@@ -0,0 +1,21 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/staircase/problem
+  * 
+  * Day 4 - Staircase - 21/03/2025
+  *
+      #
+    ##
+  ###
+####
+
+  * 
+  * 
+*/
+
+function staircase(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n-i) + "#".repeat(i))
+  }    
+}
+
+staircase(4);
