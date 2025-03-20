5ac91cb6b5a2b37673cb9cbaf74b3609890d56cc
arul2101 <myanuarullah20@gmail.com>
2025-03-20 12:44:03 +0700

    day 3 Birthday Cake Candles 20/03/2025

---
+++ #3-BirthdayCakeCandles.js
@@ -0,0 +1,25 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+  * 
+  * Day 3 - Birthday Cake Candles - 20/03/2025
+  *
+      - Anda bertugas membuat kue ulang tahun anak. Kue tersebut akan berisi satu lilin untuk setiap tahun usia total anak. Anak hanya boleh meniup lilin yang paling tinggi. Tugas Anda adalah menghitung berapa banyak lilin yang paling tinggi.
+
+  * 
+  * 
+*/
+
+function birthdayCakeCandles(candles) {
+  const maxValue = Math.max(...candles);
+  let maxValueCount = 0;
+
+  for(let i = 0; i < candles.length; i++) {
+    if(candles[i] === maxValue) {
+      maxValueCount += 1;
+    }
+  }
+
+  return maxValueCount;
+}
+
+console.log(birthdayCakeCandles([82, 49, 82, 82, 41, 82, 15, 63, 38, 25]));
