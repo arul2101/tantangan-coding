06d19b02e0fca7dd245923af3b535c56e2506644
arul2101 <myanuarullah20@gmail.com>
2025-03-27 14:19:28 +0700

    day 14 Select By Id 31/03/2025

---
+++ SQL/#3-SelectByID.sql
@@ -0,0 +1,12 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/select-by-id/problem
+  * Level : Easy
+  * 
+  * Day 14 - Select By ID - 31/03/2025
+  *
+  * 
+  * 
+*/
+
+SELECT * FROM CITY WHERE ID = 1661;
+
