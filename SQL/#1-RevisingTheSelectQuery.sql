af1a88b93759774b538aa32883797fabb1ade47d
arul2101 <myanuarullah20@gmail.com>
2025-03-27 14:13:30 +0700

    day 12 Revising The Select Query I 29/03/2025

---
+++ SQL/#1-RevisingTheSelectQuery.sql
@@ -0,0 +1,12 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/revising-the-select-query/problem
+  * Level : Easy
+  * 
+  * Day 12 - Revising the Select Query I - 29/03/2025
+  *
+  * 
+  * 
+*/
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
+
