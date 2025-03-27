0d4f69a3ec64aabd2c1c8b07955840a7cd3b82ed
arul2101 <myanuarullah20@gmail.com>
2025-03-27 14:16:31 +0700

    day 13 Revising The Select Query II 30/03/2025

---
+++ SQL/#2-RevisingTheSelectQueryII.sql
@@ -0,0 +1,12 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+  * Level : Easy
+  * 
+  * Day 13 - Revising the Select Query II - 30/03/2025
+  *
+  * 
+  * 
+*/
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000;
+
