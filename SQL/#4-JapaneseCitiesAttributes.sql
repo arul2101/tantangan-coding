f8e8be6b08a431b61cfb7174720a4b7b45b107ff
arul2101 <myanuarullah20@gmail.com>
2025-03-27 14:23:17 +0700

    day 15 Japanese Cities Attributes 01/04/2025

---
+++ SQL/#4-JapaneseCitiesAttributes.sql
@@ -0,0 +1,12 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+  * Level : Easy
+  * 
+  * Day 15 - Japanese Cities Attributes - 01/04/2025
+  *
+  * 
+  * 
+*/
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
+
