b93e7f21c8ee04bf28fed7233b0d9c4faafd2049
arul2101 <myanuarullah20@gmail.com>
2025-04-04 16:27:45 +0700

    day 18 Weather Observation Station 1 04/04/2025

---
+++ SQL/#7-WeatherObservationI.sql
@@ -0,0 +1,21 @@
+/*
+  * Link : https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+  * Level : Easy
+  * 
+  * Day 18 - Weather Observation Station 1 - 04/04/2025
+  *
+  * 
+  * 
+*/
+
+
+/*
+    Enter your query here and follow these instructions:
+    1. Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+    2. The AS keyword causes errors, so follow this convention: "Select t.Field From table1 t" instead of "select t.Field From table1 AS t"
+    3. Type your code immediately after comment. Don't leave any blank line.
+*/
+
+
+SELECT city, state FROM STATION;
+
